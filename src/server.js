7a277d6136f6c94a7d0a7b8b31b92f96f6f962bd
Andrew Rakhmanin <transylvaniadream@gmail.com>
2020-09-06 13:26:33 +0300

    deploy, fixes

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const adminRouter = require("./routes/adminRouter.js");
 require("dotenv").config();
 app.set("view engine", "pug");
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(urlencodedParser);
@@ -18,7 +18,7 @@ app.use(clientRouter);
 app.use(adminRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const port = process.env.PORT || 3006;
