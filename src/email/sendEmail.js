153d8d0fd44b3401a6dcb597798538348d5421ca
Andrew Rakhmanin <transylvaniadream@gmail.com>
2020-09-08 15:54:39 +0300

    new mail service added

--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -1,42 +1,53 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 const { pugEngine } = require("nodemailer-pug-engine");
 
-const sendEmailFunc = async (name, email, size, city, date, master, price, time) => {
-
+const sendEmailFunc = async (
+  name,
+  email,
+  size,
+  city,
+  date,
+  master,
+  price,
+  time
+) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "FastMail",
     auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
-      }
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
-  transporter.use('compile', pugEngine({
-    templateDir: __dirname + '/templates',
-    pretty: true
-  }));
+  transporter.use(
+    "compile",
+    pugEngine({
+      templateDir: __dirname + "/templates",
+      pretty: true,
+    })
+  );
 
   const options = {
-    subject: 'Your order was created!',
+    subject: "Your order was created!",
     to: `${email}`,
-    template: 'orderWasCreated',
+    template: "orderWasCreated",
     ctx: {
-      tName: name, 
-      tEmail: email, 
-      tSize: size, 
-      tCity: city, 
-      tDate: date, 
-      tMaster: master, 
-      tPrice: price, 
-      tTime: time
-    }
-  }
+      tName: name,
+      tEmail: email,
+      tSize: size,
+      tCity: city,
+      tDate: date,
+      tMaster: master,
+      tPrice: price,
+      tTime: time,
+    },
+  };
 
-  transporter.sendMail(options)
-    .then(result => console.log("MESSAGE WAS SENT"))
-    .catch(err => console.log("ERROR EMAIL SENDING", err))
-  
-}
+  transporter
+    .sendMail(options)
+    .then((result) => console.log("MESSAGE WAS SENT"))
+    .catch((err) => console.log("ERROR EMAIL SENDING", err));
+};
 
 module.exports = sendEmailFunc;
