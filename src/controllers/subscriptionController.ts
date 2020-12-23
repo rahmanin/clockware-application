277e326959dbfe0fdea31cadbb8915c0a8fde514
Undead <transylvaniadream@gmail.com>
2020-12-23 13:09:28 +0200

    Cron task to remove invalid subscriptions added

--- src/controllers/subscriptionController.ts
+++ src/controllers/subscriptionController.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from "express"
 import subscription_settings, { SubscrSetting } from '../models/subscription_settings';
-import Validator from 'validatorjs';
+import cron from 'node-cron';
+import jwt from 'jsonwebtoken';
+import { Op } from "sequelize";
 
 interface RequestWithUserData extends Request {
   userData?: {
@@ -52,6 +54,36 @@ const deleteSubscriptionSettings = (req: RequestWithUserData, res: Response) => 
       res.sendStatus(500)
     })
 }
+
+const tokenIsInvalid = (token: string) => {
+  return jwt.verify(
+    token,
+    process.env.SECRETKEY,
+    (err, decoded) => {
+      if (err) {
+        return true
+      } else {
+        return false
+      }
+    }
+  );
+}
+
+cron.schedule('0 */1 * * *', () => {
+  subscription_settings.findAll<SubscrSetting>()
+    .then(result => {
+      const subscriptionsWithInvalidToken = result.filter(e => tokenIsInvalid(e.user_token))
+      const arrayOfIds = subscriptionsWithInvalidToken.map(e => e.id)
+      subscription_settings.destroy<SubscrSetting>({
+        where: {
+          id: { [Op.in]: arrayOfIds }
+        }
+      })
+        .then(() => console.log("INVALID SUBSCRS DELETED"))
+        .catch(() => console.log("ERROR DELETE INVALID SUBSCRS"))
+  })
+    .catch(() => console.log("ERROR SUBSCRS CRON TASK"))
+})
 
 export default {
   createSubscriptionSettings,
