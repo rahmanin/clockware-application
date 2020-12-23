97fcec8fae2a2869d2f4a5af9d43a9b44535b5ee
Undead <transylvaniadream@gmail.com>
2020-12-23 11:45:10 +0200

    serviceWroker.js fixed, deploy

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -32,10 +32,10 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      // const swFileName = process.env.NODE_ENV === 'production' 
-      //   ? 'service-worker.js' 
-      //   : 'custom-sw.js'
-      const swUrl = `${process.env.PUBLIC_URL}/custom-sw.js`;
+      const swFileName = process.env.NODE_ENV === 'production' 
+        ? 'service-worker.js' 
+        : 'custom-sw.js'
+      const swUrl = `${process.env.PUBLIC_URL}/${swFileName}`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
